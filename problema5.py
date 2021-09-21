f7584704dbfae51bc47194754f4259eb91e8e1af
nicoleta <71027700+nicoleta23-bot@users.noreply.github.com>
2021-09-21 20:21:36 +0300

    Add files via upload

---
+++ problema5.py
@@ -0,0 +1,15 @@
+x=[1,2,3,4,5]
+y=sorted(x)
+print(y)
+a=[1,2,3]
+print(sum(a))
+print(sum(y))
+print((x))
+p=1
+for i in range(0,len(x)):
+    p*=x[i]
+print('produsul elementelor=', p)
+vabsol=abs(y[2])
+print('valoarea absoluta al elementului 3 = ', vabsol)
+b=[1,1]
+print(sum(b))
